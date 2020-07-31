a6ff5aeaeece5c6df4cd2ea5567deff2fe1aeb26
Anirudha <anirudha.vm@gmail.com>
2020-07-31 18:03:15 +0200

    Add comments about the variable order for script

--- middle.sh
+++ middle.sh
@@ -1,4 +1,8 @@
 # add ability to run on any file
 # add ability to work with any number of line
 
+# $1 refers to the file name
+# $2 refers to the number of lines to go with head
+# $3 refers to the number of lines to go with the tail 
+
 head -n "$2" "$1" | tail -n  "$3"
